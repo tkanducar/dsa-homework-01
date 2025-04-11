d7ba4b83d11e4e203be5eb98e9e5447fa067e01b
Teo Kanducar <129656639+tkanducar@users.noreply.github.com>
2025-04-11 13:49:41 +0200

    first commit

--- Task01/Source.cpp
+++ Task01/Source.cpp
@@ -1,21 +1,24 @@
 #include <SFML/Graphics.hpp>
+#include "Cvijet.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Hello, SFML world!");
+	window.setFramerateLimit(60);
+	Cvijet cvijet(window);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
+	while (window.isOpen())
+	{
+		while (const std::optional<sf::Event> event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+				window.close();
+		}
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+		window.clear();
+		cvijet.draw();
+		window.display();
+	}
+
+	return 0;
 }
